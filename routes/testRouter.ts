1b1a9694dc5db118507cc8f7563d7e516b8e3bcd
s1f10210273 <s1f102102739@iniad.org>
2025-05-04 21:10:16 +0900

    cookieを確認

--- routes/testRouter.ts
+++ routes/testRouter.ts
@@ -1,4 +1,11 @@
 import { Hono } from "hono";
+import { getCookie } from "hono/cookie";
 
 export const testRouter = new Hono();
 testRouter.get("/", (c) => c.text("test dir"));
+
+// Cookieを表示
+testRouter.get("/display-cookie", (c) => {
+  const cookie = getCookie(c, "cid");
+  return c.json({ cookie });
+});
