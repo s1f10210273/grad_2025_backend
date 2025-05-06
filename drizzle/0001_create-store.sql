2558bc05714a67ee7f8c4a2d50799fa3658979c6
s1f10210273 <oshikuboshuei@icloud.com>
2025-05-06 20:49:43 +0900

    マイグレーションを実行

---
+++ drizzle/0001_create-store.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `stores` (
+	`uuid` varchar(36) NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`deleted_at` timestamp,
+	CONSTRAINT `stores_uuid` PRIMARY KEY(`uuid`)
+);
