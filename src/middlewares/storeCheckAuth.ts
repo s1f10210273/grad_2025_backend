bf5dc4b1f2b633d87ffd2f41b0b6ca9abc97662d
s1f10210273 <oshikuboshuei@icloud.com>
2025-05-06 21:12:47 +0900

    middlewareを追加

---
+++ src/middlewares/storeCheckAuth.ts
@@ -0,0 +1,26 @@
+import type { MiddlewareHandler } from "hono";
+import { sessionExpirationTime } from "../helpers/const.js";
+
+export const storeCheckAuth: MiddlewareHandler = async (c, next) => {
+	const session = await c.get("session");
+
+	const uuid = session.get("uuid");
+	const role = session.get("role");
+	const expirationTime = session.get("expirationTime");
+
+	const now = Date.now();
+
+	if (
+		!uuid ||
+		!role ||
+		!expirationTime ||
+		role !== "store" ||
+		expirationTime < now
+	) {
+		await session.delete();
+		return c.json({ message: "Unauthorized" }, 401);
+	}
+
+	session.set("expirationTime", now + sessionExpirationTime);
+	next();
+};
